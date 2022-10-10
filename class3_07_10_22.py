bf447a6b6a03219677b1c9be9a6f6933dd790d39
aslamoficb <aslamkfass@gmail.com>
2022-10-10 15:12:50 +0600

    first commit

---
+++ class3_07_10_22.py
@@ -0,0 +1,57 @@
+
+'''
+
+# my_tuple = ('onion', 'potato', 'ginger')
+# print(my_tuple)
+
+# for a in my_tuple:
+    
+#     print(a)
+my_set = {'onion', 'potato', 'ginger','onion'}
+ 
+print(my_set)
+
+my_set.add('alu')
+
+print(my_set)
+   
+# my_set.remove('lau') 
+
+my_set.discard('ginger') 
+print(my_set)
+
+
+for s in my_set:
+    print(s)
+    
+
+# sum = 0
+
+# for x = 1 to 100
+
+
+
+my_dic = {'name' : 'Aslam', 'dep' : 'Science'}
+print(my_dic)
+
+
+# name = 'Sk. Aslam Uddin'
+# print(name[4:9])
+
+### 
+sum = 0
+
+for x in range(10,101,3):
+    print(x)
+    sum += x
+
+print ()    
+print(sum)
+
+
+'''
+# ### Name print from list
+# name = ['Aslam', 'Salam', 'Salim']
+# for i in range(len(name)):
+#     print("I like", name[i])
+    
---
+++ raya-zahin.py
@@ -0,0 +1,32 @@
+
+'''
+a = 'zahin'
+b = 'boy '
+
+print(a ,'is a bad', b)
+
+'''
+'''
+a='raya'
+b='girl'
+c='is a lazy'
+print( a, c ,b )
+'''
+'''
+x = 'mita'
+y = 'love'
+z = 'icecream'
+
+print (x,y,z)
+'''
+
+from re import I
+
+
+h = 'khuku'
+j = 'khalu'
+p = 'got married'
+u = 'in 2020'
+
+print (h,'and',j,p)
+
